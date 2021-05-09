15d0d5f66840447d6ffbff9c52a9c930f50b174e
Sunny Hossin <hossisun@students.zhaw.ch>
2021-05-09 16:41:22 +0200

    Fix addTickers and fetchTickers

--- amplify/backend/function/fetchTickers/src/index.py
+++ amplify/backend/function/fetchTickers/src/index.py
@@ -1,12 +1,41 @@
+import json
+import datetime
+import boto3
+import decimal
+
+
 def handler(event, context):
-  print('received event:')
-  print(event)
-  return {
-      'statusCode': 200,
-      'headers': {
-          'Access-Control-Allow-Headers': '*',
-          'Access-Control-Allow-Origin': '*',
-          'Access-Control-Allow-Methods': 'OPTIONS,POST,GET'
-      },
-      'body': "Hello from your new Amplify Python lambda!"
-  }
+
+    dynamodb = boto3.resource('dynamodb')
+    response = listTickers(dynamodb)
+
+    return {
+        'statusCode': 200,
+        'headers': {
+            'Access-Control-Allow-Headers': '*',
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Methods': 'OPTIONS,POST,GET',
+            "Content-Type": "application/json"
+        },
+        'body': json.dumps(response, default=default)
+    }
+
+
+def default(obj):
+    """
+    JSON Decimal deserializer
+    """
+    if isinstance(obj, decimal.Decimal):
+        return str(obj)
+    raise TypeError("Object of type '%s' is not JSON serializable" %
+                    type(obj).__name__)
+
+
+def listTickers(dynamodb):
+    """
+    Return all stocks
+    """
+
+    tickers = dynamodb.Table('Tickers')
+    response = tickers.scan()
+    return response['Items']
---
+++ src/app/company-add/company-add.component.ts
@@ -0,0 +1,51 @@
+import { Component, OnInit } from '@angular/core';
+import Amplify, { API } from 'aws-amplify';
+
+
+@Component({
+  selector: 'app-company-add',
+  templateUrl: './company-add.component.html',
+  styleUrls: ['./company-add.component.scss']
+})
+export class CompanyAddComponent implements OnInit {
+
+  ticker = '';
+  startDate: any;
+  endDate: any;
+
+  constructor() {}
+
+  ngOnInit(): void {
+  }
+
+  click(): void {
+    console.log("Clicked wixo");
+    API.get("fintechconnector", "/addticker", {})
+      .then(response => {
+        // Add your code here
+        console.log(response);
+      })
+      .catch(error => {
+        console.log(error.response)
+      })
+  }
+
+  inputTicker(ticker: string) {
+    this.ticker = ticker;
+  }
+
+  clearInput() {
+    this.ticker = '';
+  }
+
+  OnDateChange(datefield: string, date: string): void {
+    if (datefield == 'startDate') {
+      this.startDate = new Date(date).getTime();
+      console.log('Timestamp::' + this.startDate);
+    }
+    if (datefield == 'endDate') {
+      this.endDate = new Date(date).getTime();
+    }
+  }
+
+}
